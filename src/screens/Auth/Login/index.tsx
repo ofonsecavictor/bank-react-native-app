380b7c0b59ec909c157d461967aaad4a70558ef8
ofonsecavictor <contatovictorfonseca@gmail.com>
2023-10-05 00:52:09 -0300

    feat: background adjustments

--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {colors} from '../../../theme/theme';
 
 export function LoginScreenComponent() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: 'black'}}>Login Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.primary,
+      }}>
+      <Text style={{color: colors.lightBackground}}>Login Screen</Text>
     </View>
   );
 }
