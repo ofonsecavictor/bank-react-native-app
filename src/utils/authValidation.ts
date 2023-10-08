e4256d36ed3248861c372b1fd0f7600f813c24c2
ofonsecavictor <contatovictorfonseca@gmail.com>
2023-10-08 18:11:19 -0300

    chore: remove

--- src/utils/authValidation.ts
+++ src/utils/authValidation.ts
@@ -5,7 +5,6 @@ export const loginValidationSchema = yup.object().shape({
     .string()
     .required('Campo obrigatório')
     .test('valid-cpf-cnpj', 'CPF ou CNPJ inválido', value => {
-      // Verifique se é um CPF válido (11 dígitos) ou um CNPJ válido (14 dígitos)
       return /^[0-9]{11}$|^[0-9]{14}$/.test(value);
     }),
   password: yup.string().required('Campo obrigatório'),
